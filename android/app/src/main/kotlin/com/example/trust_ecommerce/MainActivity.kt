61c7e557c4ed9ab44a4c908c37566aac94dde3de
belal_soliman <belalsoliman8@gmail.com>
2024-08-08 18:34:31 +0300

    Initialize Flutter Project Structure and Configuration Files
    This commit sets up the initial structure and configuration files for a new Flutter project named "trust_ecommerce". It includes the creation of essential directories and files for Android, iOS, Linux, macOS, web, and Windows platforms. The commit encompasses the following key aspects:
    
    - **Project Configuration**: Initializes the project with necessary configuration files such as `.gitignore`, `analysis_options.yaml`, and `pubspec.yaml`.
    - **Platform-Specific Setup**:
      - **Android**: Adds Android-specific files including Gradle configurations, AndroidManifest.xml, and resource files.
      - **iOS**: Includes Xcode project files, Info.plist, and resource assets.
      - **Linux**: Sets up CMakeLists.txt for building the project on Linux, including Flutter-specific configurations.
      - **macOS**: Initializes the macOS project with Xcode configurations, Info.plist, and resource files.
      - **Web**: Adds web-specific files such as index.html, manifest.json, and icon assets.
      - **Windows**: Configures the Windows project with CMakeLists.txt, resource files, and Win32 window management code.
    - **Flutter Integration**: Integrates Flutter-specific files and configurations for each platform, ensuring compatibility and proper setup for Flutter development.
    - **Testing and Linting**: Includes setup for testing and linting configurations to ensure code quality and maintainability.
    
    This commit lays the foundation for further development and customization of the "trust_ecommerce" Flutter application across multiple platforms.

---
+++ android/app/src/main/kotlin/com/example/trust_ecommerce/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.trust_ecommerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("My App"),
+        ),
+        body: const Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
